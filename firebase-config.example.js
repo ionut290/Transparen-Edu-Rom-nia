183b73d0647aa22bf476db62fd3cd7edb68358c5
ionut290 <148634773+ionut290@users.noreply.github.com>
2026-09-17 07:37:10 +0200

    Add Firebase web configuration template

---
+++ firebase-config.example.js
@@ -0,0 +1,10 @@
+// Copiază acest fișier ca firebase-config.js și completează valorile din Firebase Console.
+// Nu publica chei administrative / service-account în aplicația web.
+window.TRANSPARENTA_EDU_FIREBASE = {
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
+};
